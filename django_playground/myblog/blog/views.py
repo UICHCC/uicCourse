db9fc4f9f687f79f7023ebf10a4bfae086744fe3
ecwu <bitlabwzh@gmail.com>
2018-01-30 21:30:40 +0800

    set up the first templates

--- django_playground/myblog/blog/views.py
+++ django_playground/myblog/blog/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello, World!')
+    return render(request, 'index.html', {'hello': 'Hello, Blog~'})
 
 
 
