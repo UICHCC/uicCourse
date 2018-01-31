1c5b5b9903fcbdd6ddc3c032060d435242b0729f
ecwu <bitlabwzh@gmail.com>
2018-01-31 14:50:30 +0800

    finish post edit page

--- django_playground/myblog/blog/urls.py
+++ django_playground/myblog/blog/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('article/<int:article_id>', views.article_page, name='post_page'),
-    path('edit/', views.edit_page, name='edit_page'),
+    path('edit/<int:article_id>', views.edit_page, name='edit_page'),
     path('edit/action/', views.edit_page_db, name='edit_new'),
 ]
