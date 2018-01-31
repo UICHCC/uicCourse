03163bfc663574ee61e600dadb1377038ba395f5
ecwu <bitlabwzh@gmail.com>
2018-02-01 00:33:32 +0800

    finish course solo page and course edit page

--- django_playground/uicCourse/course/urls.py
+++ django_playground/uicCourse/course/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home_page'),
+    path('?error=<int:error_number>', views.index_error),
     path('login/', views.login_receiver, name='login_receiver'),
     path('logout/', views.logout_receiver, name='logout_receiver'),
     path('course/', views.course_list, name='course_list'),
-    path('course/<int:course_code>', views.course_detail),
-    path('course/edit/<int:course_code>', views.course_edit),
+    path('course/<int:course_id>', views.course_detail, name='course_detail'),
+    path('course/edit/<int:course_id>', views.course_edit, name='course_detail_edit'),
     path('course/edit_submit', views.course_edit_submit),
 ]
--- django_playground/uicCourse/course/views.py
+++ django_playground/uicCourse/course/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
-from django.conf import settings
 
 from . import models
 
 
 def index(request):
-    return render(request, 'course/index.html')
+    if not request.user.is_authenticated:
+        return render(request, 'course/index.html')
+    else:
+        userdata = request.user
+        return redirect('/course/')
+
+
+def index_error(request, error_number):
+    if str(error_number) == '1':
+        return render(request, 'course/index.html', {'errormessage': 'Wrong Username or password.'})
+    elif str(error_number) == '2':
+        return render(request, 'course/index.html', {'errormessage': 'Logout successfully.'})
+    else:
+        return render(request, 'course/index.html', {'errormessage': "Don't Play that error number!"})
 
 
 def login_receiver(request):
@@ -18,7 +30,7 @@ def login_receiver(request):
         login(request, user)
         return redirect('/course/')
     else:
-        return HttpResponse('No way')
+        return redirect('/')
 
 
 def logout_receiver(request):
@@ -30,15 +42,27 @@ def course_list(request):
     if not request.user.is_authenticated:
         return redirect('/')
     else:
-        return HttpResponse('Login user can see this course_list')
+        userdata = request.user
+        course_all = models.Course.objects.all()
+        return render(request, 'course/course.html', {'userdata': userdata, 'courses': course_all})
 
 
-def course_detail(request):
-    return HttpResponse('This is a course detail page')
+def course_detail(request, course_id):
+    if not request.user.is_authenticated:
+        return redirect('/')
+    else:
+        userdata = request.user
+        course_solo = models.Course.objects.get(pk=course_id)
+        return render(request, 'course/detail.html', {'userdata': userdata, 'coursedata': course_solo})
 
 
-def course_edit(request):
-    return HttpResponse('This is a course edit page')
+def course_edit(request, course_id):
+    if not request.user.is_authenticated:
+        return redirect('/')
+    else:
+        userdata = request.user
+        course_solo = models.Course.objects.get(pk=course_id)
+        return render(request, 'course/detail_edit.html', {'userdata': userdata, 'coursedata': course_solo})
 
 
 def course_edit_submit(request):
