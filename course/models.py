99d577c37ba5571fd339361383e32d4804d18392
ecwu <bitlabwzh@gmail.com>
2018-03-01 09:18:33 +0800

    New models: QuickVotes

--- course/models.py
+++ course/models.py
@@ -103,3 +103,15 @@ class Comments(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+class QuickVotes(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+    vote_date = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    vote_status = models.NullBooleanField(blank=True, null=True)  # null = not vote, True = up vote, False = down vote
+    is_invalid_vote = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.id)
