a9dfbfc58f2f8456ac70b7b7991e7d4a9d487ca3
ecwu <bitlabwzh@gmail.com>
2018-03-10 01:40:57 +0800

    Modified InvitationCode

--- course/models.py
+++ course/models.py
@@ -83,7 +83,7 @@ class Course(models.Model):
 class InvitationCode(models.Model):
     invitation_code = models.TextField(null=True, unique=True)
     usability = models.BooleanField(default=True)
-    who_register = models.ForeignKey(User, on_delete=models.CASCADE)
+    who_register = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.invitation_code
