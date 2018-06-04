d1e8292d8fae1f97578728fb527d173dcdcfc591
ecwu <bitlabwzh@gmail.com>
2018-06-05 00:53:14 +0800

    finish basic user auth

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.welcome_page)
+    path('', views.welcome_page, name='welcome_page'),
+    path('login/', views.login_page, name='login_page'),
+    path('logout/', views.logout_receiver, name='logout_receiver'),
+    path('signup/', views.signup_page, name='signup_page'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -12,3 +12,29 @@ from django.http import JsonResponse
 
 def welcome_page(request):
     return render(request, 'index/index.html')
+
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.add_message(request, messages.SUCCESS, 'Login successfully.')
+            return redirect('/')
+        else:
+            messages.add_message(request, messages.ERROR, 'Wrong username or password.')
+            return redirect('/')
+    else:
+        return render(request, 'index/login.html')
+
+
+def logout_receiver(request):
+    logout(request)
+    messages.add_message(request, messages.SUCCESS, 'Logout successfully.')
+    return redirect('/')
+
+
+def signup_page(request):
+    return render(request, 'index/signup.html')
