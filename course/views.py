f5ef9d0b1f7b0caf450c054aae9c21fd2884ddfb
ecwu <bitlabwzh@gmail.com>
2018-06-07 22:00:48 +0800

    clean up some code

--- course/views.py
+++ course/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render, redirect
-# from django.http import HttpResponse
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
-from django.core.exceptions import ObjectDoesNotExist
-from django.contrib import messages
-# from django.contrib.messages import get_messages
-from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render, redirect
-# from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
-from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
-# from django.contrib.messages import get_messages
-from django.http import JsonResponse
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
 from .forms import SignUpForm
