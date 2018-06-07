2c2fde1bd5b637722560a87768c1662bd0f8ef8a
ecwu <bitlabwzh@gmail.com>
2018-06-07 22:24:19 +0800

    setup division and major models

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from . import models
 # Register your models here.
+admin.site.register(models.Division)
+
+admin.site.register(models.Major)
+
+admin.site.register(models.ValidDivisionMajorPair)
--- course/models.py
+++ course/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Division(models.Model):
+    division_en = models.CharField(max_length=128, null=True)
+    division_cn = models.CharField(max_length=128, null=True)
+    division_en_abbr = models.CharField(max_length=128, null=True)
+
+    class Meta:
+        verbose_name = _('division')
+        verbose_name_plural = _('divisions')
+
+    def __str__(self):
+        return self.division_en_abbr
+
+
+class Major(models.Model):
+    major_en = models.CharField(max_length=128, null=True)
+    major_cn = models.CharField(max_length=128, null=True)
+    major_en_abbr = models.CharField(max_length=128, null=True)
+
+    class Meta:
+        verbose_name = _('major')
+        verbose_name_plural = _('majors')
+
+    def __str__(self):
+        return self.major_en_abbr
+
+
+class ValidDivisionMajorPair(models.Model):
+    major = models.ForeignKey(Major, on_delete=models.CASCADE)
+    division = models.ForeignKey(Division, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%s %s" % (self.major.major_en, self.division.division_en)
