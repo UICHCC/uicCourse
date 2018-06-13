10cb2e15ea8defca8c695cb8160507b5566dcfda
ecwu <bitlabwzh@gmail.com>
2018-06-13 21:56:45 +0800

    add meta, help_text, and verbose_name for models

--- course/models.py
+++ course/models.py
@@ -3,9 +3,9 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Division(models.Model):
-    division_en = models.CharField(max_length=128, null=True)
-    division_cn = models.CharField(max_length=128, null=True)
-    division_en_abbr = models.CharField(max_length=128, null=True)
+    division_en = models.CharField(max_length=128, verbose_name='Division English Title')
+    division_cn = models.CharField(max_length=128, verbose_name='Division Chinese Title')
+    division_en_abbr = models.CharField(max_length=128, verbose_name='Abbreviation for Division\'s English Title')
 
     class Meta:
         verbose_name = _('division')
@@ -16,9 +16,9 @@ class Division(models.Model):
 
 
 class Major(models.Model):
-    major_en = models.CharField(max_length=128, null=True)
-    major_cn = models.CharField(max_length=128, null=True)
-    major_en_abbr = models.CharField(max_length=128, null=True)
+    major_en = models.CharField(max_length=128, verbose_name='Major English Title')
+    major_cn = models.CharField(max_length=128, verbose_name='Major Chinese Title')
+    major_en_abbr = models.CharField(max_length=128, verbose_name='Abbreviation for Major\'s English Title')
 
     class Meta:
         verbose_name = _('major')
@@ -37,24 +37,25 @@ class ValidDivisionMajorPair(models.Model):
 
 
 class CourseType(models.Model):
-    name = models.CharField(max_length=128, null=True)
-    name_abbr = models.CharField(max_length=128, null=True)
+    name = models.CharField(max_length=128, verbose_name='Type Name')
+    name_abbr = models.CharField(max_length=128, verbose_name='Type Abbreviation')
 
     def __str__(self):
         return self.name_abbr
 
 
 class Course(models.Model):
-    course_id = models.CharField(max_length=32, default='NULL0000')
-    course_name_en = models.CharField(max_length=128, null=True)
-    course_name_cn = models.CharField(max_length=128, null=True)
-    course_units = models.IntegerField(default=3)
-    course_description = models.TextField(null=True)
-    course_pre_request = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL)
-    course_type = models.ForeignKey(CourseType, default=0, on_delete=models.CASCADE)
-    course_major_take = models.ManyToManyField(ValidDivisionMajorPair)
-    is_visible = models.BooleanField(default=True)
-    is_rateable = models.BooleanField(default=True)
+    course_id = models.CharField(max_length=32, default='NULL0000', verbose_name='Course Code')
+    course_name_en = models.CharField(max_length=128, verbose_name='Course English Name')
+    course_name_cn = models.CharField(max_length=128, verbose_name='Course Chinese Name')
+    course_units = models.IntegerField(default=3, verbose_name='Course Credit', help_text='3 Units or 1 Unit')
+    course_description = models.TextField(default='', verbose_name='Course Description')
+    course_pre_request = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL,
+                                           verbose_name='Course Pre-Request')
+    course_type = models.ForeignKey(CourseType, default=0, on_delete=models.CASCADE, verbose_name='Course Type')
+    course_major_take = models.ManyToManyField(ValidDivisionMajorPair, verbose_name='Major(s) that take this course')
+    is_visible = models.BooleanField(default=True, verbose_name='Visibility of the course')
+    is_rateable = models.BooleanField(default=True, verbose_name='Rate-ability of the course')
     update_time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
--- voting/models.py
+++ voting/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 
 
 class QuickVotes(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Voting Course')
+    voter = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Voter')
     vote_date = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
     vote_status = models.NullBooleanField(blank=True, null=True)  # null = not vote, True = up vote, False = down vote
@@ -18,10 +18,11 @@ class QuickVotes(models.Model):
 
 
 class Tags(models.Model):
-    tag_title = models.CharField(max_length=128, unique=True)
-    tag_description = models.TextField(null=True)
+    tag_title = models.CharField(max_length=128, unique=True, verbose_name='Tag Title')
+    tag_description = models.TextField(default='', verbose_name='Tag Description')
     tag_opposite = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL)
-    tag_sentiment_index = models.FloatField(default=0)
+    tag_sentiment_index = models.FloatField(default=0, verbose_name='Tag Sentiment Index',
+                                            help_text='Positive number for positive emotions; Negative number for negative emotions, 0 for neutral emotion')
 
     def __str__(self):
         return self.tag_title
