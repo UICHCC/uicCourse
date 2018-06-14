0f872e3da3c0be32cb0e223fc94f179733f40a87
ecwu <bitlabwzh@gmail.com>
2018-06-14 23:42:55 +0800

    try to migrate to MySQL (failed)

--- uicCourse/settings.py
+++ uicCourse/settings.py
@@ -82,6 +82,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'django',
+        # 'USER': 'uiccourse',
+        # 'PASSWORD': 'uiccoursedb',
+        # 'USER': 'root',
+        # 'PASSWORD': '',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306',
     }
 }
 
