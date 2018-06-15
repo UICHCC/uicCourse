37de3fd4d957265ffe8911a8edace8e3db7bb92c
ecwu <bitlabwzh@gmail.com>
2018-06-15 16:48:59 +0800

    add new search function, add to course page

--- search/views.py
+++ search/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
+from course.models import Course
 # Create your views here.
+
+
+@login_required
+def search_course(request):
+    raw_input = request.GET.get('search')
+    search_result = ''
+    if raw_input is not None:
+        search_result = Course.objects.filter(Q(course_id__icontains=raw_input) | Q(course_name_en__icontains=raw_input) | Q(course_name_cn__icontains=raw_input))
+    return render(request, 'index/search.html', {'search_result': search_result, 'raw_input': raw_input})
