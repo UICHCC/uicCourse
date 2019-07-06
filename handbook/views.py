1504630292460ee55f4251c68cd3f31ff63c5617
ecwu <bitlabwzh@gmail.com>
2019-07-07 01:53:25 +0800

    finish the UI and function

--- handbook/views.py
+++ handbook/views.py
@@ -6,6 +6,15 @@ from .models import HandbookEntity
 
 # Create your views here.
 
+semester_name = {1: 'Year 1 Semester 1',
+                 2: 'Year 1 Semester 2',
+                 3: 'Year 2 Semester 1',
+                 4: 'Year 2 Semester 2',
+                 5: 'Year 3 Semester 1',
+                 6: 'Year 3 Semester 2',
+                 7: 'Year 4 Semester 1',
+                 8: 'Year 4 Semester 2'}
+
 
 def handbook(request):
     all_majors = ValidDivisionMajorPair.objects.all().order_by('division__division_en_abbr')
@@ -23,18 +32,24 @@ def handbook(request):
 def handbook_content(request, r_major, admission_year):
     is_valid = True
     handbook_info = {}
+    handbook_info_unit_sum = {}
     query_major = {}
     try:
         query_major = ValidDivisionMajorPair.objects.get(major__major_en_abbr=r_major)
         handbook_info_temp = HandbookEntity.objects.filter(major=query_major, admission_year=admission_year)
+        if not handbook_info_temp.exists():
+            is_valid = False
         for entity in handbook_info_temp:
-            if entity.study_semester in handbook_info:
-                handbook_info[entity.study_semester].append(entity)
+            if semester_name[entity.study_semester] in handbook_info:
+                handbook_info['{}'.format(semester_name[entity.study_semester])].append(entity)
+                handbook_info_unit_sum['{}'.format(semester_name[entity.study_semester])] += entity.unit
             else:
-                handbook_info[entity.study_semester] = [entity]
+                handbook_info['{}'.format(semester_name[entity.study_semester])] = [entity]
+                handbook_info_unit_sum['{}'.format(semester_name[entity.study_semester])] = entity.unit
     except ObjectDoesNotExist:
         is_valid = False
     return render(request, 'handbook/content.html', {'major': query_major,
                                                      'admission_year': admission_year,
                                                      'handbook_info': handbook_info,
-                                                     'is_valid': is_valid})
+                                                     'is_valid': is_valid,
+                                                     'unit': handbook_info_unit_sum})
