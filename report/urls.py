5a10c4616d9ff4ac89816229945afcf15d6b85d8
ecwu <bitlabwzh@gmail.com>
2019-07-08 01:43:35 +0800

    create new report module

--- report/urls.py
+++ report/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('report/', views.report, name='report'),
+    path('report/graph-api/', views.dependency_graph_api, name='graph-api')
 ]
--- report/views.py
+++ report/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+import json
 # Create your views here.
+from course.models import Course, ValidDivisionMajorPair, CourseType, Division
+from voting.models import QuickVotes, UserTaggingCourse
+
+
+def report(request):
+    content = dict()
+    content['course_type'] = CourseType.objects.all().count()
+    content['course_amount'] = Course.objects.all().count()
+    content['review_q'] = QuickVotes.objects.all().count()
+    content['review_t'] = UserTaggingCourse.objects.all().count()
+    content['major_amount'] = ValidDivisionMajorPair.objects.all().count() - 1
+    content['division_amount'] = Division.objects.all().count() - 1
+    return render(request, 'report/index.html', {'content': content})
+
+
+def dependency_graph_api(request):
+    # data = serializers.serialize("xml", Course.objects.all())
+    node_id = 0
+    edge_id = 0
+    graph = {'graph': {'nodes': [], 'edges': []}}
+    course_node = Course.objects.all()
+    all_major = ValidDivisionMajorPair.objects.all()
+    all_division = Division.objects.all()
+    for division in all_division:
+        if division.division_en_abbr == "MISC":
+            pass
+        graph['graph']['nodes'].append({'id': node_id, 'type': 'division', 'label': division.division_en})
+        node_id += 1
+    for major in all_major:
+        if major.division.division_en_abbr == "MISC":
+            pass
+        graph['graph']['nodes'].append({'id': node_id, 'type': 'major', 'label': major.major.major_en})
+        node_id += 1
+        graph['graph']['edges'].append({'id': edge_id, 'source': get_id_by_name(graph, major.division.division_en), 'target': get_id_by_name(graph, major.major.major_en)})
+        edge_id += 1
+    # return HttpResponse("Unavailable")
+    return JsonResponse(graph)
+
+
+def get_id_by_name(dictionary, name):
+    for node_item in dictionary['graph']['nodes']:
+        if node_item['label'] == name:
+            return node_item['id']
+    return False
